e760155ce8afa912f03acfe499d3a84369ad8b4b
vinotco <45597892+vinotco@users.noreply.github.com>
2018-12-10 16:02:47 +0100

    Update Tuile.java

--- l'ileInterdite/src/l/ileinterdite/Tuile.java
+++ l'ileInterdite/src/l/ileinterdite/Tuile.java
@@ -11,4 +11,31 @@ package l.ileinterdite;
  */
 public class Tuile {
     
+    private EtatTuile etatTuile;
+    private ArrayList<Aventurier> joueursSurGrille = new ArrayList();
+
+    public Tuile(EtatTuile etatTuile, ArrayList<Aventurier> joueursSurGrille) {
+        this.etatTuile = etatTuile;
+        this.joueursSurGrille = joueursSurGrille;
+    }
+
+    public EtatTuile getEtatTuile() {
+        return etatTuile;
+    }
+
+    public ArrayList<Aventurier> arriveJoueur() {
+        return joueursSurGrille;
+    }
+
+    public void setEtatTuile(EtatTuile etatTuile) {
+        this.etatTuile = etatTuile;
+    }
+
+    public void arriveeJoueur(Aventurier joueur) {
+        this.joueursSurGrille.add(joueur);
+    }
+    
+    public void departJoueur(Aventurier joueur) {
+        this.joueursSurGrille.remove(joueur);
+    }
 }
