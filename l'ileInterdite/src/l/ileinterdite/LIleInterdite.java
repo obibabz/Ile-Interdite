8011597f5432ead649b098cf7f59fbccc8a19285
satreo <satreo@pc-dg-033-12.iut2.upmf-grenoble.fr>
2018-12-17 15:33:05 +0100

    Merge origin/master

--- l'ileInterdite/src/l/ileinterdite/LIleInterdite.java
+++ l'ileInterdite/src/l/ileinterdite/LIleInterdite.java
@@ -5,6 +5,8 @@
  */
 package l.ileinterdite;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author poensing
@@ -16,6 +18,36 @@ public class LIleInterdite {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        ArrayList<Aventurier> joueursSurGrille = new ArrayList<Aventurier>();
+        
+        Tuile PAb = new Tuile("Le Pont des Abîmes", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile PBr = new Tuile("La Porte de Bronze", l.ileinterdite.EtatTuile.INONDEE);
+        Tuile COm = new Tuile("La Caverne des Ombres", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile PFe = new Tuile("La Porte de Fer", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile POr = new Tuile("La Porte d'Or", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile FOu = new Tuile("Les Falaises de l'Oubli", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile PCo = new Tuile("Le Palais de Corail", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile PAr = new Tuile("La Porte d'Argent", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile DIl = new Tuile("Les Dunes de l'Illusion", l.ileinterdite.EtatTuile.NOYEE);
+        Tuile H = new Tuile("Heliport", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile PCu = new Tuile("La Porte de Cuivre", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile JHu = new Tuile("Le Jardin des Hurlements", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile FPo = new Tuile("La Forêt Pourpre", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile LPe = new Tuile("Le Lagon Perdu", l.ileinterdite.EtatTuile.INONDEE);
+        Tuile MBr = new Tuile("Le Marais Brumeux", l.ileinterdite.EtatTuile.NOYEE);
+        Tuile O = new Tuile("Observatoire", l.ileinterdite.EtatTuile.INONDEE);
+        Tuile RFa = new Tuile("Le Rocher Fantôme", l.ileinterdite.EtatTuile.NOYEE);
+        Tuile CBr = new Tuile("La Caverne du Brasier", l.ileinterdite.EtatTuile.INONDEE);
+        Tuile TSo = new Tuile("Le Temple du Soleil", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile TLu = new Tuile("Le Temple de la Lune", l.ileinterdite.EtatTuile.NOYEE);
+        Tuile PMa = new Tuile("Le Palais des Marées", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile VCr = new Tuile("Le Val du Crépuscule", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile TGu = new Tuile("La Tour du Guet", l.ileinterdite.EtatTuile.NORMAL);
+        Tuile JMu = new Tuile("Le Jardin des Murmures", l.ileinterdite.EtatTuile.INONDEE);
+        
+        
+        
     }
     
 }
