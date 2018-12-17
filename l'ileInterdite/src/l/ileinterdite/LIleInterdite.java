18f272ba49408e4d7c494cfdbd368c7e7fc2ddf0
satreo <satreo@pc-dg-033-12.iut2.upmf-grenoble.fr>
2018-12-17 15:58:52 +0100

    Merge origin/master

--- l'ileInterdite/src/l/ileinterdite/LIleInterdite.java
+++ l'ileInterdite/src/l/ileinterdite/LIleInterdite.java
@@ -46,6 +46,34 @@ public class LIleInterdite {
         Tuile TGu = new Tuile("La Tour du Guet", l.ileinterdite.EtatTuile.NORMAL);
         Tuile JMu = new Tuile("Le Jardin des Murmures", l.ileinterdite.EtatTuile.INONDEE);
         
+        Tuile[][] grille = new Tuile[6][6];
+        
+        grille[2][0] = PAb;
+        grille[3][0] = PBr;
+        grille[1][1] = COm;
+        grille[2][1] = PFe;
+        grille[3][1] = POr;
+        grille[4][1] = FOu;
+        grille[0][2] = PCo;
+        grille[1][2] = PAr;
+        grille[2][2] = DIl;
+        grille[3][2] = H;
+        grille[4][2] = PCu;
+        grille[5][2] = JHu;
+        grille[0][3] = FPo;
+        grille[1][3] = LPe;
+        grille[2][3] = MBr;
+        grille[3][3] = O;
+        grille[4][3] = RFa;
+        grille[5][3] = CBr;
+        grille[1][4] = TSo;
+        grille[2][4] = TLu;
+        grille[3][4] = PMa;
+        grille[4][4] = VCr;
+        grille[2][5] = TGu;
+        grille[3][5] = JMu;
+        
+        Grille grid = new Grille(grille);
         
         
     }
