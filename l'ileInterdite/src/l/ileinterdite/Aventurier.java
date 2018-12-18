68b0c169e28794e4b625847f893bf72d338aee1e
vinotco <vinotco@pc-dg-039-05.iut2.upmf-grenoble.fr>
2018-12-18 13:06:37 +0100

    Merge origin/master

--- l'ileInterdite/src/l/ileinterdite/Aventurier.java
+++ l'ileInterdite/src/l/ileinterdite/Aventurier.java
@@ -48,6 +48,7 @@ public abstract class Aventurier {
     public ArrayList<Tuile> getTuilesAssechables(Grille g) {
         ArrayList<Tuile> tuilesAdj = g.getTuileAdj(position);
         ArrayList<Tuile> tuilesAssech = g.getTuilesInondees(tuilesAdj);
+        tuilesAssech.add(position);
         if(position.getEtatTuile() == EtatTuile.INONDEE) {tuilesAssech.add(position);}
         return tuilesAssech;
     }
