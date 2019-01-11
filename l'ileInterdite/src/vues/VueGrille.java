605836c1dbe1f0af60cc903bfe4ba61446680147
satreo <satreo@pc-dg-035-08.iut2.upmf-grenoble.fr>
2019-01-11 09:51:47 +0100

    Merge origin/master

--- l'ileInterdite/src/vues/VueGrille.java
+++ l'ileInterdite/src/vues/VueGrille.java
@@ -19,70 +19,62 @@ public class VueGrille extends JPanel{
     
      public VueGrille() {
     
-        JPanel panelCentral = new JPanel();
-        panelCentral.setLayout(new GridLayout(10,10));
+        super();
+        this.setLayout(new GridLayout(10,10));
 
         int i = 0;
         while (i<100) {
-            panelCentral.add(new JPanel());
+            this.add(new JPanel());
             i++;
         }
 
         int j = 0;
         while (j<100) {
-        panelCentral.getComponent(j).setBackground(Color.BLUE);
+        this.getComponent(j).setBackground(Color.BLUE);
+        this.add(new VueTuile());
         j++;
         }
 
-        panelCentral.getComponent(14).setBackground(Color.yellow);
-        panelCentral.getComponent(15).setBackground(Color.yellow);
-        panelCentral.getComponent(23).setBackground(Color.yellow);
-        panelCentral.getComponent(24).setBackground(Color.yellow);
-        panelCentral.getComponent(25).setBackground(Color.yellow);
-        panelCentral.getComponent(26).setBackground(Color.yellow);
-        panelCentral.getComponent(32).setBackground(Color.yellow);
-        panelCentral.getComponent(33).setBackground(Color.yellow);
-        panelCentral.getComponent(34).setBackground(Color.yellow);
-        panelCentral.getComponent(35).setBackground(Color.yellow);
-        panelCentral.getComponent(36).setBackground(Color.yellow);
-        panelCentral.getComponent(37).setBackground(Color.yellow);
-        panelCentral.getComponent(41).setBackground(Color.yellow);
-        panelCentral.getComponent(42).setBackground(Color.yellow);
-        panelCentral.getComponent(43).setBackground(Color.yellow);
-        panelCentral.getComponent(44).setBackground(Color.yellow);
-        panelCentral.getComponent(45).setBackground(Color.yellow);
-        panelCentral.getComponent(46).setBackground(Color.yellow);
-        panelCentral.getComponent(47).setBackground(Color.yellow);
-        panelCentral.getComponent(48).setBackground(Color.yellow);
-        panelCentral.getComponent(51).setBackground(Color.yellow);
-        panelCentral.getComponent(52).setBackground(Color.yellow);
-        panelCentral.getComponent(53).setBackground(Color.yellow);
-        panelCentral.getComponent(54).setBackground(Color.yellow);
-        panelCentral.getComponent(55).setBackground(Color.yellow);
-        panelCentral.getComponent(56).setBackground(Color.yellow);
-        panelCentral.getComponent(57).setBackground(Color.yellow);
-        panelCentral.getComponent(58).setBackground(Color.yellow);
-        panelCentral.getComponent(62).setBackground(Color.yellow);
-        panelCentral.getComponent(63).setBackground(Color.yellow);
-        panelCentral.getComponent(64).setBackground(Color.yellow);
-        panelCentral.getComponent(65).setBackground(Color.yellow);
-        panelCentral.getComponent(66).setBackground(Color.yellow);
-        panelCentral.getComponent(67).setBackground(Color.yellow);
-        panelCentral.getComponent(73).setBackground(Color.yellow);
-        panelCentral.getComponent(74).setBackground(Color.yellow);
-        panelCentral.getComponent(75).setBackground(Color.yellow);
-        panelCentral.getComponent(76).setBackground(Color.yellow);
-        panelCentral.getComponent(84).setBackground(Color.yellow);
-        panelCentral.getComponent(85).setBackground(Color.yellow);
+        this.getComponent(14).setBackground(Color.GREEN);
+        this.getComponent(15).setBackground(Color.GREEN);
+        this.getComponent(23).setBackground(Color.GREEN);
+        this.getComponent(24).setBackground(Color.GREEN);
+        this.getComponent(25).setBackground(Color.GREEN);
+        this.getComponent(26).setBackground(Color.GREEN);
+        this.getComponent(32).setBackground(Color.GREEN);
+        this.getComponent(33).setBackground(Color.GREEN);
+        this.getComponent(34).setBackground(Color.GREEN);
+        this.getComponent(35).setBackground(Color.GREEN);
+        this.getComponent(36).setBackground(Color.GREEN);
+        this.getComponent(37).setBackground(Color.GREEN);
+        this.getComponent(41).setBackground(Color.GREEN);
+        this.getComponent(42).setBackground(Color.GREEN);
+        this.getComponent(43).setBackground(Color.GREEN);
+        this.getComponent(44).setBackground(Color.GREEN);
+        this.getComponent(45).setBackground(Color.GREEN);
+        this.getComponent(46).setBackground(Color.GREEN);
+        this.getComponent(47).setBackground(Color.GREEN);
+        this.getComponent(48).setBackground(Color.GREEN);
+        this.getComponent(51).setBackground(Color.GREEN);
+        this.getComponent(52).setBackground(Color.GREEN);
+        this.getComponent(53).setBackground(Color.GREEN);
+        this.getComponent(54).setBackground(Color.GREEN);
+        this.getComponent(55).setBackground(Color.GREEN);
+        this.getComponent(56).setBackground(Color.GREEN);
+        this.getComponent(57).setBackground(Color.GREEN);
+        this.getComponent(58).setBackground(Color.GREEN);
+        this.getComponent(62).setBackground(Color.GREEN);
+        this.getComponent(63).setBackground(Color.GREEN);
+        this.getComponent(64).setBackground(Color.GREEN);
+        this.getComponent(65).setBackground(Color.GREEN);
+        this.getComponent(66).setBackground(Color.GREEN);
+        this.getComponent(67).setBackground(Color.GREEN);
+        this.getComponent(73).setBackground(Color.GREEN);
+        this.getComponent(74).setBackground(Color.GREEN);
+        this.getComponent(75).setBackground(Color.GREEN);
+        this.getComponent(76).setBackground(Color.GREEN);
+        this.getComponent(84).setBackground(Color.GREEN);
+        this.getComponent(85).setBackground(Color.GREEN);
 
-        
-        
-        
-        
-        
-        
-        
-        
-        this.add(panelCentral);
     }
 }
--- l'ileInterdite/src/vues/VueTuile.java
+++ l'ileInterdite/src/vues/VueTuile.java
@@ -1,6 +1,20 @@
 package vues;
  
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class VueTuile extends JPanel {
+    
+    private JButton bouton;
+    
+    public VueTuile() {
+        super();
+        //this.setLayout(GridLayout(5,5));
+        
+        this.add(bouton);
+        
+        
+        
+    }
+    
 }
